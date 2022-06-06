8181b61987950143d427f104f529c5edca53a5c6
Frank-197 <frankomosh197@gmail.com>
2022-06-06 21:24:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  return scuberGreetingForFeet
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  /
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
+
+
+    
+
+   
+
+    
