0c90830b3a8f4fa5a3b84c8ae75c11c39fcf80f1
Frank-197 <frankomosh197@gmail.com>
2022-06-07 13:53:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 function scuberGreetingForFeet(){
-  var returnResponse = "";
-  
+      var returnResponse = "";
   function scuberGreetingForFeet(distanceInFeet){
-    // Write your code here!
     if (distanceInFeet > 2500) {
-      returnResponse = 'No can do.';
-    }
-    else if (distanceInFeet > 2000) {
-      returnResponse = 'I will gladly take your thirty bucks.';
-    }
-    else if (distanceInFeet <= 400) {
-      returnResponse = 'This one is on me!';
-    }
-    return returnResponse;
+  returnResponse = 'No.';
   }
-  
-  function ternaryCheckCity(){
-  function ternaryCheckCity(city){
-    // Write your code here!
-    cityName === 'NYC' ? returnResponse = 'Ok, sounds good.' : returnResponse = 'No go.';
-    return returnResponse;
+  else if (distanceInFeet > 2000) {
+  returnResponse = 'I will gladly take your thirty bucks.';
   }
-  
-  function switchOnCharmFromTip(){
-  function switchOnCharmFromTip(tipAmount){
-    // Write your code here!
+  else if (distanceInFeet <= 400) {
+  returnResponse = 'Free Ride!';
+  }
+  return returnResponse;
+  }
+    function ternaryCheckCity(city){
+  // Write your code here!
+  cityName === 'NYC' ? returnResponse = 'Ok, sounds good.' : returnResponse = 'No go.';
+  return returnResponse;
+  }
+     function switchOnCharmFromTip(tipAmount){
+  // Write your code here!
   } 
-    switch (tipAmount) {
-      case 'generous' : returnResponse = 'Thank you so much.'; break;
+  switch (tipAmount) {
+     case 'generous' : returnResponse = 'Thank you so much.'; break;
       case 'not as generous': returnResponse = 'Thank you.'; break;
-      default : returnResponse = 'Bye.';
-      return returnResponse;
-    }
-    
+        default : returnResponse = 'Bye.';
+          return returnResponse;
+  }
   }
