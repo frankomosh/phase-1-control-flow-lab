0883fcc0216dd34a56d7d7c40513eb0d2ea0db35
Frank-197 <frankomosh197@gmail.com>
2022-06-10 22:58:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function scuberGreetingForFeet(){
+/*function scuberGreetingForFeet(){
       var returnResponse = "";
   function scuberGreetingForFeet(distanceInFeet){
     if (distanceInFeet > 2500) {
@@ -26,4 +26,34 @@ function scuberGreetingForFeet(){
         default : returnResponse = 'Bye.';
           return returnResponse;
   }
+  }*/
+  function scuberGreetingForFeet(feet){
+    if (feet <= 400){
+      return 'This one is on me!';
+    }
+    else if (feet > 400 && feet < 2000){
+      return 'That will be twenty bucks.'
+    }
+    else if (1999 < feet && feet < 2500){
+      return 'I will gladly take your thirty bucks.'
+    }
+    else{
+      return 'No can do.'
+    }
   }
+  function ternaryCheckCity(city){
+    // Write your code here!
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  }
+  function switchOnCharmFromTip(tip){
+    // Write your code here!
+    switch(tip){
+      case 'generous':
+        return "Thank you so much.";
+      case 'not as generous':
+        return 'Thank you.'
+      default:
+        return 'Bye.'
+    }
+  }
+    
